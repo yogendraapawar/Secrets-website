f13bd30dc2154bbca338cb7041f8f3e2f7d9d6b4
Yogendra Pawar <yogendra131994@gmail.com>
2022-08-09 21:41:17 +0530

    user

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ passport.serializeUser(function(user, done) {
 });
 
 passport.deserializeUser(function(id, done) {
-  User.findById(id, function(err, user) {
+  user.findById(id, function(err, user) {
     done(err, user);
   });
 });
