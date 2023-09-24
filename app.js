4948b48806c3c215fdd3af8ad2483e096d573be0
Yogendra Pawar <yogendra131994@gmail.com>
2023-09-24 16:27:28 +0530

    url update

--- app.js
+++ app.js
@@ -335,7 +335,7 @@ app.get("/edit/:id",(req, res)=>{
 });
 
 
-const PORT = 3000
+const PORT = process.env.PORT || 3000
   app.listen(PORT, function() {
     console.log("Server started on port 3000");
   });
